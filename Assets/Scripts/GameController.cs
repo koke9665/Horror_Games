010502135e171684678a41cec558bfa4452a1d21
koke9665 <yuuki9665@gmail.com>
2018-06-01 14:07:36 +0900

    軽量化&システム改良

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+    public GameObject Player_Object;
+    public Animator Player;
+    public float Count = 0;
+    public float Count2 = 0;
+    public int Count3 = 0;
+    public float Player_Turn_1=0;
+    public float Player_Turn_2=0;
+    public float Negative_Point = 2;
+    private bool Negative_Start;
+    private bool Negative_Start2;
+    private bool Negative_flag = true;
+    public float Turn = 0;
+    private float rotate;
+    public GameObject GameOver;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Count = Count + Time.deltaTime;
+        float Player_Rotate = Player_Object.transform.localEulerAngles.y;
+        Count2 = Count2 + Time.deltaTime;
+        if (Count2 > 1) {
+            Count3 += 1;
+            if (Negative_Start)
+            {
+                Player_Turn_1 = Player_Rotate;
+                Negative_Start = false;
+            }
+            Count2 = 0;
+        }
+
+        if (Count3 == 5) {
+            if (Negative_Start2)
+            {
+                Player_Turn_2 = Player_Rotate;
+                Negative_Start2 = false;
+            }
+            Count3 = 0;
+            Negative_flag = true ;
+        }
+
+
+        if (Negative_flag)
+        {
+            if (Player_Turn_1 < Player_Turn_2)
+            {
+                Turn = Player_Turn_2 - Player_Turn_1;
+                if (Turn > 150)
+                {
+                    Negative_Point--;
+                    Turn = 0;
+                    Negative_flag = false;
+                }
+            }
+            else if (Player_Turn_2 < Player_Turn_1)
+            {
+                Turn = Player_Turn_1 - Player_Turn_2;
+                if (Turn > 150)
+                {
+                    Negative_Point--;
+                    Turn = 0;
+                    Negative_flag = false;
+                }
+            }
+            Negative_Start = true;
+            Negative_Start2 = true;
+        }
+
+
+        if (Count<100){
+
+        }else if (Count < 200)
+        {
+            Player.SetBool("Glitch2", true);
+        } else {
+            Debug.Log("GameOver");
+            Player.SetBool("GameOver", true);
+            Invoke("GameOverTime",2F);
+        }   
+    }
+
+    void GameOverTime(){
+        GameOver.active = true;
+    }
+}
---
+++ Assets/Scripts/Start_Gate.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Start_Gate : MonoBehaviour {
+
+    public Animator Gate_left;
+    public Animator Gate_right;
+    public Animator Gritch;
+    bool start;
+
+	// Use this for initialization
+	void Start () {
+        start = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+     void OnTriggerEnter(Collider theCollider)
+    {
+        if (theCollider.tag == "Player" && start == true)
+        {
+            Debug.Log("接触したよ！");
+            Gate_left.SetBool("Open", true);
+            Gate_right.SetBool("Open", true);
+            Gritch.SetTrigger("WeakGlitch");
+            start = false;
+        }
+    }
+
+}
