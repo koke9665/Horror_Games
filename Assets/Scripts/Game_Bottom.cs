03d13bb0d71541c0d502d967f9841b326f583d4d
koke9665 <yuuki9665@gmail.com>
2018-10-23 16:18:27 +0900

    チュートリアルの作成

--- Assets/Scripts/Game_Bottom.cs
+++ Assets/Scripts/Game_Bottom.cs
@@ -5,13 +5,23 @@ using UnityEngine;
 public class Game_Bottom : MonoBehaviour {
     public GameObject Text_Title;
     public GameObject KeyBord;
+    public GameObject KeyBord_w;
+    public GameObject KeyBord_a;
+    public GameObject KeyBord_s;
+    public GameObject KeyBord_d;
     public GameObject Mouse;
+    public GameObject Mouse_Type;
     public Animator Fade;
     public Animator Controller;
 	// Use this for initialization
 	void Start () {
     KeyBord.active=false;
-    Mouse.active=false;
+    KeyBord_w.active = false;
+        KeyBord_a.active = false;
+        KeyBord_s.active = false;
+        KeyBord_d.active = false;
+        Mouse.active=false;
+    Mouse_Type.active = false;
 	}
 
 	// Update is called once per frame
@@ -24,7 +34,12 @@ public class Game_Bottom : MonoBehaviour {
 	}
     void Start_Animator(){
         KeyBord.active=true;
+        KeyBord_w.active = true;
+        KeyBord_a.active = true;
+        KeyBord_s.active = true;
+        KeyBord_d.active = true;
         Mouse.active=true;
+        Mouse_Type.active = true;
         Fade.SetBool("Fade", true);
         Fade.SetBool("Start", false);
     }
--- Assets/Scripts/Scene_move.cs
+++ Assets/Scripts/Scene_move.cs
@@ -5,16 +5,69 @@ using UnityEngine.SceneManagement;
 public class Scene_move : MonoBehaviour {
 
     public GameObject black;
+    public Animator W_Key;
+    public Animator A_Key;
+    public Animator S_Key;
+    public Animator D_Key;
+    public Animator Mouse;
 
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        //Wキーボード入力判定
+        if(Input.GetKey(KeyCode.W)){
+            W_Key.SetBool("W_Anime", true);
+        }else if(Input.GetKeyUp(KeyCode.W)){
+            W_Key.SetBool("W_Anime", false);
+        }
+
+        //Aキーボード入力判定
+        if (Input.GetKey(KeyCode.A))
+        {
+            A_Key.SetBool("A_Anime", true);
+        }
+        else if (Input.GetKeyUp(KeyCode.A))
+        {
+            A_Key.SetBool("A_Anime", false);
+        }
+
+        //Sキーボード入力判定
+        if (Input.GetKey(KeyCode.S))
+        {
+            S_Key.SetBool("S_Anime", true);
+        }
+        else if (Input.GetKeyUp(KeyCode.S))
+        {
+            S_Key.SetBool("S_Anime", false);
+        }
+
+        //Dキーボード入力判定
+        if (Input.GetKey(KeyCode.D))
+        {
+            D_Key.SetBool("D_Anime", true);
+        }
+        else if (Input.GetKeyUp(KeyCode.D))
+        {
+            D_Key.SetBool("D_Anime", false);
+        }
+
+        //マウス入力判定
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Mouse.SetBool("Mouse_Anime", true);
+        }
+        else if (Input.GetButtonUp("Fire1"))
+        {
+            Mouse.SetBool("Mouse_Anime", false);
+        }
+
+    }
 
     void OnTriggerEnter(Collider other)
 	{
